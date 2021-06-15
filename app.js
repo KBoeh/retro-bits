9be31eb60c3502cd41a9cbb78ba3ae7eebd67249
Karl Boehringer <79428505+KBoeh@users.noreply.github.com>
2021-06-14 20:57:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const hamburger = document.querySelector(".hamB");
 const navLinks = document.querySelector(".navLinks");
-const html = document.querySelector('html');
+const html = document.querySelector("html");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
-    html.classList.toggle('-is-locked');
+    html.classList.toggle("-is-locked");
     window.scrollTo(0, 0);
 });
